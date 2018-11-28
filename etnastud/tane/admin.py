ef90e37c2cc41513d4b43a4d27d50a9f57f50b9b
Theo Massard <massar_t@etna-alternance.net>
2018-11-28 14:31:42 +0100

    (tane) add admin models and migrations

--- etnastud/tane/admin.py
+++ etnastud/tane/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from tane.models import Student, Promotion
 
-# Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+class PromotionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Promotion, PromotionAdmin)
--- etnastud/tane/models.py
+++ etnastud/tane/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    promotion = models.IntegerField()
+    login = models.CharField(max_length=10)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+    )
+    promotion = models.ForeignKey(
+        'Promotion',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return '{} - {}'.format(self.login, self.promotion.name)
+
+
+class Promotion(models.Model):
+    promo_id = models.IntegerField()
+    name = models.CharField(max_length=50)
