4d5436bfaa89bfc357c2466520f4762f53e2fe72
Theo Massard <massar_t@etna-alternance.net>
2018-11-28 14:30:57 +0100

    (app) add base app and jet plugin

--- etnastud/etnastud/settings.py
+++ etnastud/etnastud/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tane',
 ]
 
 MIDDLEWARE = [
